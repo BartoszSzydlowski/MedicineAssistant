27a78aae92394b08c67c5ce00a0f358ff4d511bc
Bartosz Szydłowski <bbartek311@gmail.com>
2021-01-14 21:43:05 +0100

    Edited token

--- MedicineAssistant.Web/Token/JwtTokenInfo.cs
+++ MedicineAssistant.Web/Token/JwtTokenInfo.cs
@@ -1,29 +1,31 @@
-﻿using System.IdentityModel.Tokens.Jwt;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Net.Http.Headers;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 
 namespace MedicineAssistant.Web.Token
 {
 	public static class JwtTokenInfo
 	{
-		//public static string GetUserIdFromToken(HttpContext httpContext)
-		//{
-		//	var token = httpContext.Request.Headers[HeaderNames.Authorization].ToString().Replace("Bearer ", "");
-		//	var handler = new JwtSecurityTokenHandler();
-		//	var jsonToken = handler.ReadToken(token);
-		//	var tokenS = handler.ReadToken(token) as JwtSecurityToken;
-		//	var userId = tokenS.Claims.First(x => x.Type == JwtRegisteredClaimNames.Sub).Value;
-		//	return userId;
-		//}
-
-		public static string Token { get; set; }
-
-		public static string GetUserIdFromToken()
+		public static string GetUserIdFromToken(HttpContext httpContext)
 		{
+			var token = httpContext.Request.Headers[HeaderNames.Authorization].ToString().Replace("Bearer ", "");
 			var handler = new JwtSecurityTokenHandler();
-			var jsonToken = handler.ReadToken(Token);
-			var decodedToken = handler.ReadToken(Token) as JwtSecurityToken;
-			var userId = decodedToken.Claims.First(x => x.Type == JwtRegisteredClaimNames.Sub).Value;
+			var jsonToken = handler.ReadToken(token);
+			var tokenS = handler.ReadToken(token) as JwtSecurityToken;
+			var userId = tokenS.Claims.First(x => x.Type == JwtRegisteredClaimNames.Sub).Value;
 			return userId;
 		}
+
+		//public static string Token { get; set; }
+
+		//public static string GetUserIdFromToken()
+		//{
+		//	var handler = new JwtSecurityTokenHandler();
+		//	var jsonToken = handler.ReadToken(Token);
+		//	var decodedToken = handler.ReadToken(Token) as JwtSecurityToken;
+		//	var userId = decodedToken.Claims.First(x => x.Type == JwtRegisteredClaimNames.Sub).Value;
+		//	return userId;
+		//}
 	}
 }
